04606aa9385cb4a4719851b6686dc4056ddc8db7
MeBhargavi <patelbhargavi1624@gmail.com>
2023-08-28 23:11:02 +0530

    NEW

--- src/Q_1.java
+++ src/Q_1.java
@@ -1,5 +1,22 @@
-public class Q_1 {
+import java.util.Scanner;
+
+ class Q_1 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of array:");
+        int n = sc.nextInt();
+        int i;
+
+        int a[] = new int[n];
+
+        System.out.println("Enter Elements of array:");
+        for( i=0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("After Reversed");
+        for ( i = a.length-1;i>=0;i--){
+            System.out.println(a[i]+" ");
+        }
     }
 }
