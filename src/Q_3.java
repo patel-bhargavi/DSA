38f230972d3cf3ddf9eef4665634da203be1e06d
MeBhargavi <patelbhargavi1624@gmail.com>
2023-08-30 16:30:05 +0530

    NEW

---
+++ src/Q_3.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Q_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Size of array:");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+
+        System.out.println("Enter Elements:");
+        for (int i = 0;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+
+        System.out.println("Enter value of K:");
+        int k = sc.nextInt();
+
+        kthMax(a,k);
+        kthMin(a,k);
+
+    }
+
+    static void sortArray(int a[]){
+        for (int i = 0; i < a.length; i++) {
+            int j = i;
+            while (j > 0 && a[j - 1] > a[j]) {
+                int temp = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+
+    static void kthMax(int a[],int k){
+        sortArray(a);
+        System.out.println(k+ " Max:"+a[a.length - k]);
+    }
+
+    static void kthMin(int a[],int k){
+        sortArray(a);
+        System.out.println(k+" Min"+a[k-1]);
+    }
+
+}
