fbe1c978d63accf7e8be9fb18429761acba5e332
MeBhargavi <patelbhargavi1624@gmail.com>
2023-09-01 01:05:51 +0530

    NEW

--- src/Q_4.java
+++ src/Q_4.java
@@ -1,7 +1,26 @@
 //Given an array which consists of only 0, 1 and 2. Sort the array without using any sorting algo
 
+import java.util.Scanner;
+
 public class Q_4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        int a[] = {0,1,0,1,2,1,2};
+        int zero=0,one=0,two=0;
+
+        for(int i=0;i<a.length;i++){
+            if(a[i] == 0) zero++;
+            if(a[i] == 1) one++;
+            if(a[i] == 2) two++;
+        }
+
+        for (int i =0 ;i<zero;i++) a[i] = 0;
+        for (int i = zero; i<one+zero;i++) a[i] = 1;
+        for(int i=one+zero;i<a.length;i++) a[i] = 2;
+
+        for(int i=0;i<a.length;i++) {
+            System.out.println(a[i]);
+        }
     }
 }
---
+++ src/Q_5.java
@@ -0,0 +1,2 @@
+//Move all the negative elements to one side of the array
+
