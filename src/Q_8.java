956ae42ccb7a32ebebdc15f7ddce10a22b66423c
MeBhargavi <patelbhargavi1624@gmail.com>
2023-09-08 00:18:16 +0530

    NEW

--- src/Q_8.java
+++ src/Q_8.java
@@ -7,9 +7,15 @@ public class Q_8 {
     public static void main(String[] args) {
         int sum =0;
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements in array:");
+        int n = sc.nextInt();
 
+        int arr[] = new int[n];
 
-        int arr[] = {5,4,-1,7,8};
+        System.out.println("Enter Elements of Array:");
+        for (int i = 0 ;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
         int maxi = arr[0];
 
         for(int i =0 ; i<arr.length;i++){
